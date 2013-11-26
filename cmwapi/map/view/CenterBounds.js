bcf97470d8a616942afb88ab5d361a35a3b5e4b9
lmizenextcen <lawerence.mize@nextcentury.com>
2013-11-26 15:25:16 -0500

    Adding initial implementation of a few other view channel modules and their associated unit tests.

---
+++ cmwapi/map/view/CenterBounds.js
@@ -0,0 +1,164 @@
+define(["cmwapi/Channels", "cmwapi/Validator", "cmwapi/map/Error"], 
+    function(Channels, Validator, Error) {
+
+    /**
+     * The CenterBounds module provides methods for centering a map on a bounding box via an OWF Eventing channel
+     * according to the [CMWAPI 1.1 Specification](http://www.cmwapi.org).  This module 
+     * abstracts the OWF Eventing channel mechanism from client code and validates messages
+     * using specification rules.  Any errors are published
+     * on the map.error channel using an {@link module:cmwapi/map/Error|Error} module.
+     *
+     * According to the 
+     * CMWAPI Specification payloads sent over the channel may require validation of individual parameters or
+     * default values for omitted parameters.  Where possible, this module abstracts those rules from client code.
+     * Both the send and addHandler functions will auto-fill defaults for missing parameters. Further, addHandler
+     * will wrap any passed-in function with payload validation code, so that they fail fast on invalid payloads and
+     * do not push bad data into any map specific handlers.  A summary of payload errors is pushed to the 
+     * {@link module:cmwapi/map/Error|Error} channel if that occurs.
+     *
+     * @exports cmwapi/map/view/CenterBounds
+     */
+    var CenterBounds = {
+
+        /**
+         * Send information that centers a map on a bounding box.
+         * @param {Object|Array} data 
+         * @param {object} data.bounds Information about the bounding view.
+         * @param {object} data.bounds.southWest The southwest corner object with attributes
+         * @param {number} data.bounds.southWest.lat A latitude value
+         * @param {number} data.bounds.southWest.lon A longitude value
+         * @param {object} data.bounds.northEast The northeast corner object with attributes
+         * @param {number} data.bounds.northEast.lat A latitude value
+         * @param {number} data.bounds.northEast.lon A longitude value
+         * @param {number|"auto"} [data.zoom] A range in meters at which to set a zoom or the term "auto" to
+         *     to ask a map to best vit the overlay in the user's viewable area.
+         */
+        send : function ( data ) {
+
+            // validData will story results from any Validator and may be resused for internal
+            // error bookkeeping.
+            var validData = Validator.validObjectOrArray( data );
+            var payload = validData.payload;
+
+            // If the data was not in proper payload structure, an Object or Array of objects, 
+            // note the error and return.
+            if (!validData.result) {
+                Error.send( OWF.getInstanceId(), Channels.MAP_VIEW_CENTER_BOUNDS, data, 
+                    validData.msg);
+                return;
+            }
+
+            // Check all the feature objects; fill-in any missing attributes.
+            for (var i = 0; i < payload.length; i ++) {
+                // Check for a valid center bounds.
+                var validBounds = Validator.validBounds(payload[i].bounds);
+                if (!validBounds.result) {
+                    validData.result = false;
+                    validData.msg += validBounds.msg + " ";
+                }
+
+                // Zoom is optional; if it doesn't exist, explicitly set it to the default.
+                if (typeof payload[i].zoom !== "undefined") {
+                    payload[i].zoom = (payload[i].zoom) ? payload[i].zoom : false;
+                    if (!Validator.isNumber(payload[i].zoom) && payload[i].zoom.toString().toLowerCase() !== "auto") {
+                        validData.result = false;
+                        validData.msg += "zoom values must be a number or 'auto'. ";
+                    }
+                }
+            }
+
+            // Send along the payload if we did not fail validation.    
+            if (validData.result) {
+                if (payload.length === 1) {
+                    OWF.Eventing.publish(Channels.MAP_VIEW_CENTER_BOUNDS, Ozone.util.toString(payload[0]));
+                }
+                else {
+                    OWF.Eventing.publish(Channels.MAP_VIEW_CENTER_BOUNDS, Ozone.util.toString(payload));
+                }
+            }
+            else {
+                Error.send( OWF.getInstanceId(), Channels.MAP_VIEW_CENTER_BOUNDS, 
+                    Ozone.util.toString(data),
+                    validData.msg);
+            }
+
+        },
+
+        /**
+         * Subscribes to the center bounds channel and registers a handler to be called when messages
+         * are published to it.
+         *
+         * @param {module:cmwapi/map/view/CenterBounds~Handler} handler An event handler for any creation messages.
+         *
+         */
+        addHandler : function (handler) {
+
+            // Wrap their handler with validation checks for API for folks invoking outside of our calls
+            var newHandler = function( sender, msg ) {
+              
+                // Parse the sender and msg to JSON.
+                var jsonMsg = (Validator.isString(msg)) ? Ozone.util.parseJson(msg) : msg;
+                var data = (Validator.isArray(jsonMsg)) ? jsonMsg : [jsonMsg];
+                var validData = {result: true, msg: ""};
+
+                for (var i = 0; i < data.length; i ++) {
+                    // Check for a valid center bounds.
+                    var validBounds = Validator.validBounds(data[i].bounds);
+                    if (!validBounds.result) {
+                        validData.result = false;
+                        validData.msg += validBounds.msg + " ";
+                    }
+
+                    // Zoom is optional; if it doesn't exist, explicitly set it to the default.
+                    if (typeof data[i].zoom !== "undefined") {
+                        data[i].zoom = (data[i].zoom) ? data[i].zoom : false;
+                        if (!Validator.isNumber(data[i].zoom) && data[i].zoom.toString().toLowerCase() !== "auto") {
+                            validData.result = false;
+                            validData.msg += "zoom values must be a number or 'auto'. ";
+                        }
+                    }
+                }
+
+                if (validData.result) {
+                    handler(sender, (data.length === 1) ? data[0] : data);
+                }
+                else {
+                    Error.send(sender, Channels.MAP_VIEW_CENTER_BOUNDS, 
+                        msg,
+                        validData.msg);
+                }
+                
+            };
+
+            OWF.Eventing.subscribe(Channels.MAP_VIEW_CENTER_BOUNDS, newHandler);
+            return newHandler;
+        },
+
+        /**
+         * Stop listening to the channel and handling events upon it.
+         */
+        removeHandlers : function() {
+            OWF.Eventing.unsubscribe(Channels.MAP_VIEW_CENTER_BOUNDS);
+        }
+
+        /**
+         * A function for handling channel messages.
+         * @callback module:cmwapi/map/view/CenterBounds~Handler
+         * @param {string} sender The widget sending a format message
+         * @param {Object|Array} data  A data object or array of data objects.
+         * @param {object} data.bounds Information about the bounding view.
+         * @param {object} data.bounds.southWest The southwest corner object with attributes
+         * @param {number} data.bounds.southWest.lat A latitude value
+         * @param {number} data.bounds.southWest.lon A longitude value
+         * @param {object} data.bounds.northEast The northeast corner object with attributes
+         * @param {number} data.bounds.northEast.lat A latitude value
+         * @param {number} data.bounds.northEast.lon A longitude value
+         * @param {number|"auto"} [data.zoom] A range in meters at which to set a zoom or the term "auto" to
+         *     to ask a map to best vit the overlay in the user's viewable area.
+         */
+
+    };
+
+    return CenterBounds;
+
+});
---
+++ test/spec/cmwapi/map/view/CenterBounds.spec.js
@@ -0,0 +1,187 @@
+define(["cmwapi/Channels", "cmwapi/map/view/CenterBounds", "cmwapi/map/Error", "cmwapi/Validator",
+    "test/mock/OWF", "test/mock/Ozone"], 
+    function(Channels, CenterBounds, Error, Validator, OWF, Ozone) {
+
+    describe(Channels.MAP_VIEW_CENTER_BOUNDS + " module", function() {
+
+        var INSTANCE_ID = OWF.getInstanceId();
+
+        beforeEach(function() {
+            
+            // Mock the necessary OWF methods and attach them to the window.
+            // OWF should be in global scope when other libraries attempt to
+            // access it.
+            window.OWF = OWF;
+            window.Ozone = Ozone;
+        });
+
+        afterEach(function() {
+            // Remove our mock objects from the window so neither they nor
+            // any spies upon them hang around for other test suites.
+            delete window.OWF;
+            delete window.Ozone;
+        });
+
+        it("sends data to the correct channel", function() {
+            var eventing = OWF.Eventing;
+            expect(eventing).not.toBe(null);
+
+            spyOn(CenterBounds, 'send').andCallThrough();
+            spyOn(eventing, 'publish');
+            spyOn(Error, 'send');
+
+            CenterBounds.send({bounds: { southWest: { lat: 1, lon: 1}, northEast: {lat: 1, lon: 1}}});
+            expect(CenterBounds.send).toHaveBeenCalled();
+
+            // expect publish to be called
+            expect(eventing.publish).toHaveBeenCalled();
+            expect(eventing.publish.mostRecentCall.args[0]).toEqual(Channels.MAP_VIEW_CENTER_BOUNDS);
+
+            var payload = Ozone.util.parseJson(eventing.publish.mostRecentCall.args[1]);
+            expect(payload.bounds.southWest.lat).toEqual(1);
+            expect(payload.bounds.southWest.lon).toEqual(1);
+            expect(payload.bounds.northEast.lat).toEqual(1);
+            expect(payload.bounds.northEast.lon).toEqual(1);
+            expect(payload.zoom).toBeUndefined();
+
+            // don't expect error to be called
+            expect(Error.send.calls.length).toEqual(0);
+        });
+
+        it("fails data with invalid zoom values", function() {
+            var eventing = OWF.Eventing;
+            expect(eventing).not.toBe(null);
+
+            spyOn(CenterBounds, 'send').andCallThrough();
+            spyOn(eventing, 'publish');
+            spyOn(Error, 'send').andCallThrough();
+
+            CenterBounds.send({location: {lat: 1, lon: 2}, zoom:{}});
+            expect(CenterBounds.send).toHaveBeenCalled();
+
+            // expect publish to be called on the error channel.
+            expect(eventing.publish).toHaveBeenCalled();
+            expect(eventing.publish.mostRecentCall.args[0]).toEqual(Channels.MAP_ERROR);
+
+            CenterBounds.send({bounds: { southWest: { lat: 1, lon: 1}, northEast: {lat: 1, lon: 1}},
+                zoom:"test"});
+            expect(CenterBounds.send).toHaveBeenCalled();
+
+            // expect publish to be called on the error channel.
+            expect(eventing.publish).toHaveBeenCalled();
+            expect(eventing.publish.mostRecentCall.args[0]).toEqual(Channels.MAP_ERROR);
+
+            expect(Error.send.calls.length).toEqual(2);
+        });
+
+        it("fails data with missing bounds values", function() {
+            var eventing = OWF.Eventing;
+            expect(eventing).not.toBe(null);
+
+            spyOn(CenterBounds, 'send').andCallThrough();
+            spyOn(eventing, 'publish');
+            spyOn(Error, 'send').andCallThrough();
+
+            CenterBounds.send({});
+            expect(CenterBounds.send).toHaveBeenCalled();
+
+            // expect publish to be called on the error channel.
+            expect(eventing.publish).toHaveBeenCalled();
+            expect(eventing.publish.mostRecentCall.args[0]).toEqual(Channels.MAP_ERROR);
+            expect(Error.send.calls.length).toEqual(1);
+        });
+
+        it("unsubscribes the correct channel when removeHandlers is called", function() {
+
+            var eventing = OWF.Eventing;
+
+            spyOn(CenterBounds, 'removeHandlers').andCallThrough();
+            spyOn(Error, 'send');
+            spyOn(eventing, 'unsubscribe');
+
+            CenterBounds.removeHandlers();
+            expect(CenterBounds.removeHandlers).toHaveBeenCalled();
+            expect(eventing.unsubscribe.mostRecentCall.args[0]).toEqual(Channels.MAP_VIEW_CENTER_BOUNDS);
+
+            expect(Error.send.calls.length).toEqual(0);
+
+        });
+
+        it("wraps added handlers and validates a zoom range", function() {
+
+            var eventing = OWF.Eventing;
+            spyOn(eventing, 'subscribe');
+
+            var testHandler = jasmine.createSpy('testHandler');
+            var newHandler = CenterBounds.addHandler(testHandler);
+            expect(eventing.subscribe.mostRecentCall.args[0]).toEqual(Channels.MAP_VIEW_CENTER_BOUNDS);
+
+            // Test the behavior for newHandler  CenterBounds a sender an empty payload to pass along
+            // Our code should fill in the payload and pass it along to the testHandler.
+            var jsonVal = {
+                bounds: { southWest: { lat: 1, lon: 1}, northEast: {lat: 1, lon: 1}},
+                zoom: 10000
+            };
+            var sender = {
+                id: INSTANCE_ID
+            };
+
+            // Spy on Error and call our wrapper handler.
+            spyOn(Error, 'send');
+            newHandler(Ozone.util.toString(sender), jsonVal); 
+
+            // We don't expect error to be called
+            expect(Error.send.calls.length).toEqual(0);
+
+            // We DO expect testHandler to have been called and the missing jsonVal items to have been
+            // filled in.
+            expect(testHandler.calls.length).toEqual(1);
+            expect(testHandler.mostRecentCall.args[1].bounds.southWest.lat).toEqual(1);
+            expect(testHandler.mostRecentCall.args[1].bounds.southWest.lon).toEqual(1);
+            expect(testHandler.mostRecentCall.args[1].bounds.northEast.lat).toEqual(1);
+            expect(testHandler.mostRecentCall.args[1].bounds.northEast.lon).toEqual(1);
+            expect(testHandler.mostRecentCall.args[1].zoom).toEqual(10000);
+        });
+
+        it("passes object arrays to added handlers and validates a zoom range", function() {
+
+            var eventing = OWF.Eventing;
+            spyOn(eventing, 'subscribe');
+
+            var testHandler = jasmine.createSpy('testHandler');
+            var newHandler = CenterBounds.addHandler(testHandler);
+            expect(eventing.subscribe.mostRecentCall.args[0]).toEqual(Channels.MAP_VIEW_CENTER_BOUNDS);
+
+            // Test the behavior for newHandler  Create a sender an empty payload to pass along
+            // Our code should fill in the payload and pass it along to the testHandler.
+            var jsonVal = [{
+                bounds: { southWest: { lat: 1, lon: 1}, northEast: {lat: 1, lon: 1}},
+                zoom: 1000
+            },{
+                bounds: { southWest: { lat: 2, lon: 2}, northEast: {lat: 2, lon: 2}},
+                zoom: "auto"
+            }];
+            var sender = {
+                id: INSTANCE_ID
+            };
+
+            // Spy on Error and call our wrapper handler.
+            spyOn(Error, 'send');
+            newHandler(Ozone.util.toString(sender), jsonVal); 
+
+            // We don't expect error to be called
+            expect(Error.send.calls.length).toEqual(0);
+
+            // We DO expect testHandler to have been called and the jsonVal values to
+            // carry through unchanged.  Any missing featureId should be filled in.
+            expect(testHandler.calls.length).toEqual(1);
+            expect(testHandler.mostRecentCall.args[1].length).toEqual(2);
+            expect(testHandler.mostRecentCall.args[1][0].zoom).toEqual(1000);
+            expect(testHandler.mostRecentCall.args[1][0].bounds.southWest.lat).toEqual(1);
+            expect(testHandler.mostRecentCall.args[1][0].bounds.southWest.lon).toEqual(1);
+            expect(testHandler.mostRecentCall.args[1][1].zoom).toEqual("auto");
+            expect(testHandler.mostRecentCall.args[1][1].bounds.southWest.lat).toEqual(2);
+            expect(testHandler.mostRecentCall.args[1][1].bounds.southWest.lon).toEqual(2);
+        });
+    });
+});
