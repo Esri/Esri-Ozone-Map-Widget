4fa3f31d9af7b46cefdd1e15ea10af4fce6d0e24
Micah Fulton <micah.fulton@nextcentury.com>
2013-12-03 16:51:03 -0500

    Updated overlay manager pop-out

--- owf-map-widget/js/app.js
+++ owf-map-widget/js/app.js
@@ -15,6 +15,7 @@ require([
         });
 
         var dropZone = Dom.byId("map");
+        console.log(dropZone);
         var owf_adapter = new OWFAdapter(On , dropZone);
         
         geocoder = new Geocoder({ 
@@ -34,11 +35,34 @@ require([
 
         $('#basemaps').on('click', function() {
             toggleBaseMaps();
-            toggleOverlay(true);
         }); 
         $('#overlay').on('click', function() {
             toggleOverlay();
-            toggleBaseMaps(true);
+        });
+        $('#overlay-add-icon').on('click', function() {
+            toggleOverlaySettings();
+            $('#overlay-manager-add').toggleClass('hidden');
+            $('#overlay-manager-subtitle').text('Add New Feature');
+            $('#popover_overlay_wrapper').css('height', '305px');
+        });
+        $('#overlay-delete-icon').on('click', function() {
+            toggleOverlaySettings();
+            $('#overlay-manager-subtitle').text('Delete Existing Overlays');
+        });
+        $('#overlay-back-icon').on('click', function() {
+            $('#overlay-manager-add').toggleClass('hidden');
+            toggleOverlaySettings();
+            
+        });
+        $('form').find('input').keyup(function() {
+            var emptyInputLength = $('form > div > div > input').filter(function() {
+                return $(this).val() === ''; 
+            }).length;
+            var buttonActive = (emptyInputLength === 0 &&  $('#overlay-manager-add-button').hasClass('disabled')) ||
+                (emptyInputLength > 0 && !$('#overlay-manager-add-button').hasClass('disabled'));
+            if(buttonActive) {
+                $('#overlay-manager-add-button').toggleClass('disabled');
+            } 
         });
 
         var kmlUrl = "http://www.dgs.maryland.gov/ISSSD/FuelManagement/FuelingSites.kml";
@@ -55,9 +79,10 @@ require([
         {
             label: 'node1',
             image: './sampleimage.png',
+            type: 'feature', 
             children: [
                 { label: 'child1' },
-                { label: 'child2' }
+                { label: 'Aggrevated Assault/ No Firearm ' }
             ]
         },
         {
@@ -71,10 +96,12 @@ require([
         var $tree = $('#overlay-tree');
         $tree.tree({
             data: data,
+            dragAndDrop:true,
             autoOpen: 1,
             onCreateLi: function(node, $li) {
                 $li.find('.jqtree-title').before(
-                    '<input type="checkbox" class ="tree-node"/>'
+                    '<input type="checkbox" class ="tree-node"/>' +
+                    '<img src="http://img0056.popscreencdn.com/103222765_watchmen-smiley-1-pin-button-badge-magnet-moore-gibbons-.jpg" alt="Overlay Icon" height="25" width="25">'
                 );
             }
         });
@@ -85,29 +112,31 @@ require([
         });
 
 
-        var toggleBaseMaps = function(close) {
-            !close ? $('#basemaps').toggleClass('selected') : false;
-            if(($('#popover_content_wrapper').css('visibility') === 'visible') || close) {
-                $('#popover_content_wrapper').css('visibility', 'hidden');
-
-            } else {
-                $('#popover_content_wrapper').css('visibility', 'visible');
-                if($('#overlay').hasClass('selected')) {
-                    toggleOverlay();
-                }
+        var toggleBaseMaps = function() {
+            if(!$('#popover_overlay_wrapper').hasClass('hidden')) {
+                $('#popover_overlay_wrapper').toggleClass('hidden');
+                $('#overlay').toggleClass('selected');
             }
+            $('#popover_content_wrapper').toggleClass('hidden');
+            $('#basemaps').toggleClass('selected');
         }
        
-        var toggleOverlay = function(close) {
-            !close ? $('#overlay').toggleClass('selected') : false;
-            if(($('#popover_overlay_wrapper').css('visibility') === 'visible')|| close) {
-                $('#popover_overlay_wrapper').css('visibility', 'hidden');
-            } else {
-                $('#popover_overlay_wrapper').css('visibility', 'visible');
-                if($('#basemaps').hasClass('selected')) {
-                    toggleBaseMaps();
-                }
+        var toggleOverlay = function() {
+            if(!$('#popover_content_wrapper').hasClass('hidden')) {
+                $('#popover_content_wrapper').toggleClass('hidden');
+                $('#basemaps').toggleClass('selected');
             }
+            $('#popover_overlay_wrapper').toggleClass('hidden');
+            $('#overlay').toggleClass('selected');
+        }
+
+        var toggleOverlaySettings = function() {
+            $('#overlay-tree').toggleClass('hidden');
+            $('#overlay-manager').toggleClass('hidden');
+            $('#overlay-add-icon').toggleClass('hidden');
+            $('#overlay-delete-icon').toggleClass('hidden');
+            $('#overlay-back-icon').toggleClass('hidden');
+            $('#overlay-vr').css('right', '36px');
         }
 
     });
