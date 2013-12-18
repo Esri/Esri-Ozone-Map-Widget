d8fb3d9be208fa414fd22ba7f8ae4f2c72c5e48f
lmizenextcen <lawerence.mize@nextcentury.com>
2013-12-18 10:06:59 -0500

    Adding initial grunt targets and plugins to support a deploy version of the repo.  'grunt deploy' will now run the tests, generate docs, copy the widgets to a deployment directory, and generate minified versions of the cmwapi and cmwapi-adapter folders in the deployment directory.

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,6 +44,30 @@ module.exports = function(grunt) {
             }
         },
 
+        uglify: {
+            options: {
+              banner: '<%= pkg.license %>'
+            },
+            cmwapi: {
+                files: [{ 
+                    src: 'cmwapi/**/*.js',  // source files mask
+                    dest: 'deployment',    // destination folder
+                    expand: true,    // allow dynamic building
+                    flatten: false   // remove all unnecessary nesting
+                    //ext: '.min.js'   // replace .js to .min.js
+                }]
+            },
+            cmwapi_adapter: {
+                files: [{ 
+                    src: 'cmwapi-adapter/**/*.js',  // source files mask
+                    dest: 'deployment',    // destination folder
+                    expand: true,    // allow dynamic building
+                    flatten: false   // remove all unnecessary nesting
+                    //ext: '.min.js'   // replace .js to .min.js
+                }]
+            }
+        },
+
         copy: {
             main: {
                 files: [
@@ -54,6 +78,20 @@ module.exports = function(grunt) {
 
                   ]
              },
+             deployment: {
+                files: [
+                    // includes files within path and its sub-directories
+                  {expand: true, src: ['basic-map-widget/**'], dest: 'deployment'},
+                  {expand: true, src: ['contacts/**'], dest: 'deployment'},
+                  {expand: true, src: ['owf-map-widget/**'], dest: 'deployment'}
+                ]
+             }
+        },
+
+        clean : {
+            deployment : {
+                src : [ "deployment/**" ]
+            }
         }
     });
 
@@ -61,6 +99,8 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
     // Workaround for allowing tasks that can fail but should not abort the build chains.
     // Reference: http://stackoverflow.com/questions/16612495/continue-certain-tasks-in-grunt-even-if-one-fails
@@ -87,4 +127,8 @@ module.exports = function(grunt) {
 
     // Default task(s)
     grunt.registerTask('default', ['usetheforce_on', 'jshint', 'usetheforce_restore', 'karma', 'jsdoc'/**, 'copy'**/]  );
+
+    // Deployment task
+    grunt.registerTask('deploy', ['clean:deployment', 'usetheforce_on', 'jshint', 'usetheforce_restore', 
+        'karma:unit', 'jsdoc', 'uglify', 'copy:deployment']);
 };
