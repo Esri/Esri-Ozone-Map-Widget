7c02f5c7e72d4afff41415f9de4753b46969fd6a
Micah Fulton <micah.fulton@nextcentury.com>
2013-12-31 11:21:26 -0500

    Made it so that the add overlay/feature windows would have a scroll bar as necessary depending on screen size.

--- owf-map-widget/js/app.js
+++ owf-map-widget/js/app.js
@@ -3,11 +3,11 @@
 // NOTE: Modules that are not compatible with asynchronous module loading
 // (AMD) are included in the webapp's HTML file to prevent issues.
 require([
-    "models/map", "models/legend", "dojo/mouse", "dojo/on", "dojo/dom", "esri/dijit/Scalebar",
+    "models/map", "dojo/mouse", "dojo/on", "dojo/dom", "esri/dijit/Scalebar",
     "dojo/json", "esri/dijit/Geocoder", "esri/layers/KMLLayer","esri/dijit/BasemapGallery",
     "esri/arcgis/utils","dojo/parser","dojo/dom-style", "cmwapi-adapter/cmwapi-adapter",
     "dojo/domReady!"],
-    function(Map, Legend, Mouse, On, Dom, Scalebar, JSON, Geocoder, KMLLayer, BasemapGallery, arcgisUtils, parser, domStyle, cmwapiAdapter) {
+    function(Map, Mouse, On, Dom, Scalebar, JSON, Geocoder, KMLLayer, BasemapGallery, arcgisUtils, parser, domStyle, cmwapiAdapter) {
         var map = new Map("map", {
             center: [-76.809469, 39.168101],
             zoom: 7,
@@ -39,6 +39,24 @@ require([
                 map:map,
                 attachTo:"bottom-left",
                 scalebarUnit: "dual"
+            });
+
+            var changeAddScrollState = function() {
+                var diff = $(window).height() - $("#overlay-manager-add")[0].scrollHeight;
+                if(!$('#add-overlay-div').is(':visible') && $('#add-feature-div').is(':visible')) {
+                     $("#overlay-manager-add").css("height", $('#add-feature-div').height() + 15);
+                }
+                if(diff < 210 || $("#overlay-manager-add")[0].scrollHeight != $("#overlay-manager-add").height()) {
+                     $("#overlay-manager-add").css("height", $(window).height() - 220);
+                }
+                if($('#add-overlay-div').is(':visible') && !$('#add-feature-div').is(':visible')) {
+                    $("#overlay-manager-add").css("height", $('#add-overlay-div').height() + 15);
+                }
+                resizeOverlayManager();
+            };
+
+            $(window).bind("resize",function() {
+                changeAddScrollState();
             });
 
             $('#overlay-tree').tree({
@@ -339,6 +357,7 @@ require([
                 $('.init').hide();
                 $('.add').show();
                 resizeOverlayManager();
+                changeAddScrollState();
             };
 
             var setStateAdd = function() {
