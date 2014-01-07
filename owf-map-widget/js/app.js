3800307b0e7c492b20629ee1c239542f6a8efecd
Micah Fulton <micah.fulton@nextcentury.com>
2014-01-07 12:20:30 -0500

    Broke basemap gallery into its own files

--- owf-map-widget/js/app.js
+++ owf-map-widget/js/app.js
@@ -3,52 +3,34 @@
 // NOTE: Modules that are not compatible with asynchronous module loading
 // (AMD) are included in the webapp's HTML file to prevent issues.
 require([
-    "models/map", "models/overlayManager", "esri/dijit/Scalebar","dojo/json", "esri/dijit/Geocoder",
-    "esri/layers/KMLLayer","esri/dijit/BasemapGallery","esri/arcgis/utils","dojo/parser","dojo/dom-style",
+    "models/map", "models/overlayManager", "models/basemapGallery", "esri/dijit/Scalebar","dojo/json", "esri/dijit/Geocoder",
+    "esri/layers/KMLLayer","esri/arcgis/utils","dojo/parser","dojo/dom-style",
      /*"OWFWidgetExtensions/owf-widget-extended",*/ "dojo/domReady!"],
-    function(Map, OverlayManager, Scalebar, JSON, Geocoder, KMLLayer, BasemapGallery, arcgisUtils, parser,
+    function(Map, OverlayManager, BasemapGallery,Scalebar, JSON, Geocoder, KMLLayer, arcgisUtils, parser,
         domStyle, cmwapiAdapter, Graphic, PictureMarkerSymbol, Point) {
+
         var map = new Map("map", {
             center: [-76.809469, 39.168101],
             zoom: 7,
             basemap: "streets"
         });
-    if (OWF.Util.isRunningInOWF()) {
 
-        OWF.ready(function () {
-            OWF.notifyWidgetReady();
-
-            var geocoder = new Geocoder({
+        var geocoder = new Geocoder({
                 map: map
             }, "search");
-            geocoder.startup();
+        geocoder.startup();
 
-            var basemapGallery = new BasemapGallery({
+        var basemapGallery = new BasemapGallery({
                 showArcGISBasemaps: true,
                 map: map
-            }, "basemapGallery");
-            basemapGallery.startup();
+        }, "basemapGallery");
 
-            basemapGallery.on("error", function(msg) {
-                console.log("basemap gallery error:  ", msg);
-            });
+        new Scalebar({ map:map, attachTo:"bottom-left", scalebarUnit: "dual" });
 
-            var overlayManager = new OverlayManager(map);
-
-            new Scalebar({
-                map:map,
-                attachTo:"bottom-left",
-                scalebarUnit: "dual"
-            });
-
-            var toggleBaseMaps = function() {
-                $('#popover_content_wrapper').toggle();
-                $('#overlay').removeClass('selected');
-                $('#popover_overlay_wrapper').hide();
-                $('#basemaps').toggleClass('selected');
-            };
-
-            var addButtonHandlers = function() {
+        if (OWF.Util.isRunningInOWF()) {
+            OWF.ready(function () {
+                OWF.notifyWidgetReady();
+                var overlayManager = new OverlayManager(map);
                 $('#map').on('mouseup', function() {
                     $('#popover_overlay_wrapper').hide();
                     $('#popover_content_wrapper').hide();
@@ -56,9 +38,8 @@ require([
                     $('#overlay').removeClass('selected');
                 });
                 $('#overlay').on('click', overlayManager.toggleOverlayManager);
-                $('#basemaps').on('click', toggleBaseMaps);
+                $('#basemaps').on('click', basemapGallery.toggleBasemapGallery);
                 $("[rel=tooltip]").tooltip({ placement: 'bottom'});
-            }();
-       });
-    }
+           });
+        }
     });
