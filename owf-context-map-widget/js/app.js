fec22283110fd5b23f16db58ffad0996c192cd87
Micah Fulton <micah.fulton@nextcentury.com>
2014-02-13 16:31:36 -0500

    Added suport for viewing extents from other maps in the context map.

--- owf-context-map-widget/js/app.js
+++ owf-context-map-widget/js/app.js
@@ -22,14 +22,22 @@
  */
 
 require([
-    "esri/map", "esri/toolbars/draw", "esri/geometry/webMercatorUtils", "cmwapi/cmwapi", "dojo/domReady!"],
-    function(Map, Draw, webMercatorUtils, CommonMapApi) {
+    "esri/map", "esri/toolbars/draw", "esri/graphic", "esri/geometry/Extent", "esri/symbols/SimpleFillSymbol","esri/symbols/SimpleLineSymbol",
+    "esri/layers/GraphicsLayer", "dojo/_base/Color", "esri/geometry/webMercatorUtils", "cmwapi/cmwapi", "dojo/domReady!"],
+    function(Map, Draw, Graphic, Extent, SimpleFillSymbol, SimpleLineSymbol, GraphicsLayer, Color, webMercatorUtils, CommonMapApi) {
+        var clickEvent, doubleClickEvent;
+
         var map = new Map("mapDiv", {
             center: [-77.035841, 38.901721],
             zoom: 1,
             basemap: "streets",
             slider:false
         });
+
+        var extentColors = [new Color([255,0,0,1]), new Color([0,255,0,1]), new Color([0,0,255,1])];
+        var mapIds = {};
+        var mapColorCount = {};
+        var contextCount = 0;
 
         var sendClickEvent = function(e) {
             var locationEvent = {};
@@ -58,13 +66,71 @@ require([
             if (OWF.Util.isRunningInOWF()) {
                 OWF.ready(function () {
                     OWF.notifyWidgetReady();
-                    map.on('dbl-click', sendClickEvent);
-                    map.on('click', sendClickEvent);
+                    setHandlers();
                     draw.on('draw-end', sendDragEvent);
                 });
             }
         };
 
+        var setHandlers = function() {
+            doubleClickEvent = map.on('dbl-click', sendClickEvent);
+            clickEvent = map.on('click', sendClickEvent);
+        };
+
+        var removeHandlers = function() {
+            doubleClickEvent.remove();
+            clickEvent.remove();
+        };
+
+
+        OWF.Eventing.subscribe('map.status.view', function(source,msg) {
+            var sourceID = OWF.Util.parseJson(source).id;
+            var msgJson = OWF.Util.parseJson(msg);
+
+            var northEast = msgJson.bounds.northEast;
+            var southWest = msgJson.bounds.southWest;
+            var extent = webMercatorUtils.geographicToWebMercator(new Extent(
+                southWest.lon,
+                southWest.lat,
+                northEast.lon,
+                northEast.lat,
+                map.spatialReference));
+
+            var layer = new GraphicsLayer();
+            if(mapIds[sourceID.toString()]) {
+                map.removeLayer(mapIds[sourceID.toString()]);
+                mapIds[sourceID.toString()] = layer;
+            } else {
+                mapIds[sourceID.toString()] = layer;
+                mapColorCount[sourceID.toString()] = contextCount++;
+            }
+
+
+            var symbol = function(colorCount) {
+                return new SimpleFillSymbol(
+                    SimpleFillSymbol.STYLE_SOLID,
+                    new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID, extentColors[colorCount], 1),
+                    new Color([125,125,125,0.35]));
+            };
+
+            layer.on('mouse-over', function() {
+                removeHandlers();
+                map.setMapCursor("pointer");
+
+            });
+            layer.on('mouse-out', function() {
+                setHandlers();
+                map.setMapCursor("crosshair");
+            });
+            console.log("**************************************");
+            console.log(mapColorCount[sourceID.toString()]);
+            console.log("**************************************");
+
+            layer.add(new Graphic(extent, symbol(mapColorCount[sourceID.toString()])));
+            map.addLayer(layer);
+        });
+
         map.on('load', initMap);
+
     }
 );
